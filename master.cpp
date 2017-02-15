3512c38e79cdd570c1295d37b819531cd5f380e3
sak <sakraindra@126.com>
2017-02-15 16:55:08 +0800

    add master.cpp modify test_merg.cpp

---
+++ master.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int master(int abc , char* argv[])
+{
+	printf("test master");
+
+}
--- test_merg.cpp
+++ test_merg.cpp
@@ -1,8 +1,9 @@
-#include<>
+#include<stdio.h>
+#include<stdlib.h>
 int main(){
 
 	int test_newbranch;
-	
+	int ThisisMasterVar;
 	return 0;
 
 }
